e488f1186467c1abed6600c24f91bbf1d01cb461
igormatheusdiniz <igor.pinheiro@ccc.ufcg.edu.br>
2021-03-16 20:58:27 -0300

    config inicial do projeto

---
+++ src/main/java/com/sitemaPonto/api/SistemaPontoApplication.java
@@ -0,0 +1,14 @@
+package com.sitemaPonto.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPontoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Teste");
+		SpringApplication.run(SistemaPontoApplication.class, args);
+	}
+
+}
